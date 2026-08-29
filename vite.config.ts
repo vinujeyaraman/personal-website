62f06df807ae069492b583fd0949954fd285e2d1
peace_keeper <vinujeyaraman@gmail.comgit config --global user.email vinujeyaraman@gmail.com>
2026-08-29 19:40:16 +0530

    Configure for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personal-website/',
   plugins: [react()],
   resolve: {
     alias: {
